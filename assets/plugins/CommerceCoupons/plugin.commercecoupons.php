db07ed50d4f7640dd9a81f46ee5744b04970b08c
webber12 <web-ber12@yandex.ru>
2019-11-27 20:55:49 +0300

    small format fix

--- assets/plugins/CommerceCoupons/plugin.commercecoupons.php
+++ assets/plugins/CommerceCoupons/plugin.commercecoupons.php
@@ -6,41 +6,41 @@ if (!defined('MODX_BASE_PATH')) {
 $e = $modx->event;
 $events = ['OnWebPageInit', 'OnCollectSubtotals', 'OnOrderSaved'];
 if (in_array($e->name, $events)) {
-	include_once MODX_BASE_PATH . "assets/plugins/CommerceCoupons/controllers/CommerceCouponsController.php";
-	$controller = \CommerceCoupons\CommerceCouponsController::getInstance($params);
+    include_once MODX_BASE_PATH . "assets/plugins/CommerceCoupons/controllers/CommerceCouponsController.php";
+    $controller = \CommerceCoupons\CommerceCouponsController::getInstance($params);
 }
 
 switch ($e->name) {
-	case 'OnWebPageInit':
-		$controller->regScripts();
-		break;
-		
+    case 'OnWebPageInit':
+        $controller->regScripts();
+        break;
+        
     case 'OnCollectSubtotals': 
-		$discount = $controller->getDiscount();
-		if (!empty($discount)) {
-			$summ = 0;
-			switch ($discount['type']) {
-				case 'percent':
-					$summ = $params['total'] * $discount['amount'] / 100;
-					break;
-				case 'summ':
-					$summ = $discount['amount'];
-					break;
-				break;
-			}
-			$params['total'] -= $summ;
-			$params['rows']['discount'] = [
+        $discount = $controller->getDiscount();
+        if (!empty($discount)) {
+            $summ = 0;
+            switch ($discount['type']) {
+                case 'percent':
+                    $summ = $params['total'] * $discount['amount'] / 100;
+                    break;
+                case 'summ':
+                    $summ = $discount['amount'];
+                    break;
+                break;
+            }
+            $params['total'] -= $summ;
+            $params['rows']['discount'] = [
                 'title' => 'Скидка по купону <b>' . $discount['code'] . '</b>',
                 'price' => -$summ,
             ];
-		}
+        }
         break;
-		
-	case 'OnOrderSaved':
-		$controller->rememberOrder($params);
-		$discount = $controller->destroyCoupon();
-		break;
-		
-	default:
-		break;
+        
+    case 'OnOrderSaved':
+        $controller->rememberOrder($params);
+        $discount = $controller->destroyCoupon();
+        break;
+        
+    default:
+        break;
 }
